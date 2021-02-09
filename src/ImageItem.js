9e31a91ca85fd64a2c86e5e4b6be4cf9ca873faa
Jake Thrasher <mjakethrasher@gmail.com>
2021-02-09 14:14:10 -0800

    create file structure

---
+++ src/ImageItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ImageItem extends Component {
+    render() {
+        return (
+          <li>
+              
+          </li>
+        )
+    }
+}
