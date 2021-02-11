f14572b3569c64f12af0f9bbfbbd9e5b45826250
Jake Thrasher <mjakethrasher@gmail.com>
2021-02-10 16:31:00 -0800

    create dropdown component and pass props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import images from './data.js'
 import Header from './Header/Header.js'
 import './App.css'
 import ImageList from './ImageList.js'
+import Dropdown from './Dropdown.js'
 
 
 
@@ -10,21 +11,35 @@ import ImageList from './ImageList.js'
 export default class App extends Component {
   state = {
     keyWord:'',
+    horns:'',
   }
   handleCreatureSelect =(e)=>{
     this.setState({
       keyWord:e.target.value
     })
   }
+  handleHornsSelect =(e)=>{
+    this.setState({
+      horns:Number(e.target.value)
+    })
+  }
   render() {
     
-  // console.log(this.state.keyWord)
   const filteredImages = images.filter( (image) => {
-    if(image.keyword === this.state.keyWord){
-      return image;
-    }else if(this.state.keyWord === ''){
-      return images;
+    if(!this.state.keyWord && !this.state.horns)return true;
+
+    if (this.state.keyWord && !this.state.horns) {
+      if (image.keyword === this.state.keyWord) return true;
     }
+      
+    if (this.state.horns && !this.state.keyWord) {
+      if (image.horns === this.state.horns) return true;
+    }
+
+    if(this.state.keyWord === image.keyword && this.state.horns === image.horns){
+      return true;
+    } 
+    return false
   })
   
     return (
@@ -33,31 +48,18 @@ export default class App extends Component {
 
         <Header/>
 
+        <p className="select-prompt">Select A Creature</p>
 
-        <form>
-          <p className="select-prompt">Select A Creature</p>
+        <Dropdown  handleChange={this.handleCreatureSelect} currentValue={this.state.keyWord} options={['unicorn', 'rhino','narwhal', 'markhor', 'unilego','triceratops','chameleon','lizard', 'dragon','mouflon']}/>
 
-          <select value={this.state.keyWord}
-          onChange={this.handleCreatureSelect}>
+        <p className="select-prompt">Select Number of Horns</p>
 
-            <option value=''>select one</option>
-            <option value="narwhal">Narwhal</option>
-            <option value="rhino">Rhino</option>
-            <option value="unicorn">Unicorn</option>
-            <option value="unilego">UniLego</option>
-            <option value="markhor">Markhor</option>
-            <option value="mouflon">Mouflon</option>
-            <option value="chameleon">Chameleon</option>
-            <option value="lizard">Lizard</option>
-            <option value="dragon">Dragon</option>
-            <option value="triceratops">Triceratops</option>
-          </select>
-        </form>
+        <Dropdown  handleChange={this.handleHornsSelect} currentValue={this.state.horns} options={[1,2,3,100]} />
+
         <div className="image-container">
-        
         <ImageList images={ filteredImages } />
-
         </div>
+
       </div>
     )
   }
